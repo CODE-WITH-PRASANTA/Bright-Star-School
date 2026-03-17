c3ff017c268bef750d7936a876b1a7adc4496935
swarnaprava-06032005 <150228737+swarnaprava-06032005@users.noreply.github.com>
2026-03-17 12:22:25 +0530

    topbar details, navbar logo font change, sidebar photo, content add , gallery section and vision mission section done.

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
 
-import logo from "../../assets/Logo.webp";
+import logo from "../../assets/BSM-removebg.png";
 
 import {
   FaFacebookF,
@@ -9,130 +9,110 @@ import {
   FaLinkedinIn,
   FaYoutube,
   FaPhoneAlt,
-  FaMapMarkerAlt
+  FaMapMarkerAlt,
 } from "react-icons/fa";
 
 import { FaArrowUp } from "react-icons/fa";
 
 const Footer = () => {
-
   const scrollTop = () => {
     window.scrollTo({
-      top:0,
-      behavior:"smooth"
-    })
-  }
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <footer className="footer">
-
       <div className="footer-container">
-
         {/* LEFT SECTION */}
         <div className="footer-about">
-
           <div className="footer-logo">
-
-            <img src={logo} alt="logo"/>
-
-           
-
+            <img src={logo} alt="logo" />
           </div>
 
           <p>
-            We are constantly expanding the range of services
-            offered, taking care of children of all ages.
+            We are constantly expanding the range of services offered, taking
+            care of children of all ages.
           </p>
-
         </div>
-
 
         {/* MAIN CAMPUS */}
 
         <div className="footer-campus">
-
           <h3>Main Campus</h3>
 
           <div className="footer-line"></div>
 
           <p>
-            <FaMapMarkerAlt className="footer-icon"/>
-            First Floor, 10A Chandos Street London
-            New Town W1G 9LE
+            <FaMapMarkerAlt className="footer-icon" />
+            Plot No. 657/1094, Haridaspur <br />
+            Naharakanta, Bhubaneswar <br />
+            Odisha - 752101, India
           </p>
 
           <p className="footer-phone">
-            <FaPhoneAlt className="footer-icon"/>
-            +44 (0) 207 689 7888
+            <FaPhoneAlt className="footer-icon" />
+            7016201096
           </p>
-
         </div>
-
 
         {/* NATIONAL CAMPUS */}
 
         <div className="footer-campus">
-
           <h3>National Campus</h3>
 
           <div className="footer-line"></div>
 
           <p>
-            <FaMapMarkerAlt className="footer-icon"/>
-            Unit 6G/j Red Rose Court Sunnyhurst Road
-            Blackburn, Manchester
+            <FaMapMarkerAlt className="footer-icon" />
+            Plot No. 657/1094, Haridaspur <br />
+            Naharakanta, Bhubaneswar <br />
+            Odisha - 752101, India
           </p>
 
           <p className="footer-phone">
-            <FaPhoneAlt className="footer-icon"/>
-            +44 (0) 207 689 7634
+            <FaPhoneAlt className="footer-icon" />
+            7016201096
           </p>
-
         </div>
-
       </div>
-
 
       {/* BOTTOM BAR */}
 
       <div className="footer-bottom">
-
-        <p>
-          Copyright © 2025 <strong>Kiddino</strong>. 
-          All Rights Reserved By <span>Vecuro</span>
+        <p className="footer-copy">
+          © 2026 <strong>Bright Stars Montessori - Bhubaneswar</strong>. All
+          Rights Reserved. | Powered by Bright Stars Montessori | Developed by{" "}
+          <span>PR WEBSTOCK</span>
         </p>
 
         <div className="footer-social">
-
           <div className="footer-socialIcon">
-            <FaFacebookF/>
+            <FaFacebookF />
           </div>
 
           <div className="footer-socialIcon">
-            <FaTwitter/>
+            <FaTwitter />
           </div>
 
           <div className="footer-socialIcon">
-            <FaLinkedinIn/>
+            <FaLinkedinIn />
           </div>
 
           <div className="footer-socialIcon">
-            <FaYoutube/>
+            <FaYoutube />
           </div>
-
         </div>
-
       </div>
-
 
       {/* SCROLL BUTTON */}
 
       {/* <div className="footer-scrollTop" onClick={scrollTop}>
         <FaArrowUp/>
       </div> */}
-
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
